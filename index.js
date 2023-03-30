9b4826fc65ea37a36bd64f4c6265555309a1ebd1
charchit <charchitkhandelwal45@gmail.com>
2023-03-30 21:25:48 +0530

    added

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./Config/db");
+const { userRouter } = require("./Routes/User.Route");
+const { productRoute } = require("./Routes/Product.Route");
+const { AdminRouter } = require("./Routes/Admin.Route");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+
+app.use("/admin",AdminRouter);
+app.use("/users", userRouter);
+
+app.use("/products", productRoute);
+
+
+
+app.listen(7500, async () => {
+  try {
+    await connection;
+    console.log("Connected to DB");
+  } catch (error) {
+    console.log(" Cannot Connected to DB");
+    console.log(error);
+  }
+  console.log("Running the server at port 7500");
+});
